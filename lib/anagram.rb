100fca89e908fefccb83471f0fff04acb160cf56
Nadav Schwartz <nadavschwartz58@gmail.com>
2020-06-28 20:26:25 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -3,11 +3,20 @@ class Anagram
   attr_accessor :name
   
   def initialize(word)
+<<<<<<< HEAD
     @name = word
   end
   
   def match(array)
     array.select { |a| a.split("").sort == @name.split("").sort}
+=======
+    @name = word.split("").sort
+  end
+  
+  def match(array)
+    array_sorted = array.split("").sort
+    array_sorted.each {|a| a == @name}
+>>>>>>> c23a23b3b9a2cc3de92bd528a6e9a18314b181b1
   end
   
 end
